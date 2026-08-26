f779d95d0128b392faae357ed7198d282da52cf8
Matthew Coder <matthewcoder@tutamail.com>
2026-08-26 14:47:15 +0300

    chore(config): fix css path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2026-08-26',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
-  css: ['~/assets/css/main.css'],
+  css: ['~/src/assets/css/main.css'],
 });
